ace4ee229d1f1cb4486a5f79c62eba4b119f5d38
Matthew DeHaven <dehavenm@gmail.com>
2019-05-30 00:17:03 +0000

    Added button read commands to example code lcd.cpp

--- example/lcd.cpp
+++ example/lcd.cpp
@@ -18,7 +18,7 @@ int main() {
 
   lcd.home();
 
-  lcd.setBacklight(BLUE);
+  lcd.setBacklight(WHITE);
 
   lcd.cursor();
 
@@ -26,6 +26,40 @@ int main() {
 
 
   lcd.print("Hello, world!\nJetson Xavier");
+
+  uint8_t button_data;
+
+  while (true) //press Ctrl-C to exit
+  {
+    //read button data
+    button_data = lcd.readButtons();
+
+    if (button_data) {
+    lcd.clearDisplay();
+    lcd.setCursor(0,0);
+    if (button_data & BUTTON_UP) {
+      lcd.print("UP ");
+      lcd.setBacklight(RED);
+    }
+    if (button_data & BUTTON_DOWN) {
+      lcd.print("DOWN ");
+      lcd.setBacklight(YELLOW);
+    }
+    if (button_data & BUTTON_LEFT) {
+      lcd.print("LEFT ");
+      lcd.setBacklight(GREEN);
+    }
+    if (button_data & BUTTON_RIGHT) {
+      lcd.print("RIGHT ");
+      lcd.setBacklight(TEAL);
+    }
+    if (button_data & BUTTON_SELECT) {
+      lcd.print("SELECT ");
+      lcd.setBacklight(VIOLET);
+    }
+    }
+
+  }
 
 
 
